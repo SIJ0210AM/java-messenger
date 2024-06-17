3a7f1a904d6290adfab29c885a2cd4a4ff8424c4
andre <a.rahmaturov@mail.ru>
2024-06-17 22:15:32 +0300

    feat: Create base application project

---
+++ src/main/java/ru/sij0210am/messenger/JavaMessengerApplication.java
@@ -0,0 +1,13 @@
+package ru.sij0210am.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sij0210am/messenger/JavaMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sij0210am.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
